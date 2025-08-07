3a9d7b7e9c3d23382d852348cac39e7113473fc9
shaghayegh <bazrafkan.sh@gmail.com>
2025-08-07 16:13:12 +0330

    style: refine responsiveness and UI for homepage and footer on develop branch

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import { memo } from "react";
-import img from "../../assets/img/bal.png";
+import img from "../../assets/img/wingfotros.png";
 import logo1 from "../../assets/img/mojavez-footer.png";
 import footer from "../../assets/img/footer.png";
 
@@ -10,46 +10,55 @@ function Footer() {
   return (
     <div classname="footer">
       <div className="h-[3rem]"></div>
-      <div className=" flex justify-between h-[340px]">
-        <div>
-          <div className="flex flex-col justify-between px-[18px] h-[88%] gap-[20px]">
-            <h2 className="relative w-[157px] px-[3px] text-[175%] font-bold text-left">
-              <img src={img} className="w-[46px] bottom-[13px] absolute" />
-              ف&#x0640;&#x0640;ط&#x0640;&#x0640;&#x0640;روس
-            </h2>
-            <ul>
-              <li>
-                <Link>درباره ما</Link>
-              </li>
-              <li>
-                <Link>تماس با ما</Link>
-              </li>
-              <li>
-                <Link>سوالات متداول</Link>
-              </li>
-              <li>
-                <Link>قوانین و مقررات</Link>
-              </li>
-              <li>
-                <Link>شرایط بازگشت کالا</Link>
-              </li>
-            </ul>
-            <div className="flex gap-[13px] ">
-              <button className="p-[5px] border-solid border-red border-[2px] rounded-full">
-                <FaInstagram />
-              </button>
-              <button className="p-[5px] border-solid border-red border-[2px] rounded-full">
-                <FaTelegramPlane />
-              </button>
-              <button className="p-[5px] border-solid border-red border-[2px] rounded-full">
-                <MdOutlinePhone />
-              </button>
-            </div>
-            <img src={logo1} alt="" className="h-[2.5rem] self-start" />
-            <div className="h-[2rem]"></div>{" "}
+      <div className=" flex justify-around h-[340px]">
+        <div className="flex flex-col w-[45%] justify-between px-[18px] h-[88%] gap-[20px]">
+          <h2 className="relative w-[157px] px-[3px] text-[175%] font-bold text-left">
+            <img src={img} className="w-[46px] bottom-[13px] absolute" />
+            ف&#x0640;&#x0640;ط&#x0640;&#x0640;&#x0640;روس
+          </h2>
+          <ul>
+            <li>
+              <Link>درباره ما</Link>
+            </li>
+            <li>
+              <Link>تماس با ما</Link>
+            </li>
+            <li>
+              <Link>سوالات متداول</Link>
+            </li>
+            <li>
+              <Link>قوانین و مقررات</Link>
+            </li>
+            <li>
+              <Link>شرایط بازگشت کالا</Link>
+            </li>
+          </ul>
+          <div className="flex gap-[13px] ">
+            <button className="p-[5px] border-solid border-red border-[2px] rounded-full">
+              <FaInstagram />
+            </button>
+            <button className="p-[5px] border-solid border-red border-[2px] rounded-full">
+              <FaTelegramPlane />
+            </button>
+            <button className="p-[5px] border-solid border-red border-[2px] rounded-full">
+              <MdOutlinePhone />
+            </button>
           </div>
+          <img src={logo1} alt="" className="h-[2.5rem] self-start md:hidden" />
+          <div className="h-[2rem]"></div>{" "}
         </div>
-        <img src={footer} alt="" className="h-[94%] self-end" />
+        <img
+          src={footer}
+          alt=""
+          className="h-[94%] md:h-[100%] w-[50%] md:w-[unset] self-end"
+        />
+      </div>
+      <div className="h-[4rem] hidden md:flex items-center justify-center w-full my-[20px]">
+      <img
+        src={logo1}
+        alt=""
+        className="h-[100%]"
+      />
       </div>
     </div>
   );
