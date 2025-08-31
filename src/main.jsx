2e193311d7cccc0872fadb766ad14be82be6e694
shaghayegh <bazrafkan.sh@gmail.com>
2025-08-31 16:34:40 +0330

    feat: Add CartContext and FavContext, update AxiosContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { AxiosProvider } from "./context/AxiosContaext/AxiosContaext";
+import { FavProvider } from "./context/FavProvider/FavProvider";
+import { CartProvider } from "./context/CartContext/CartContext";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AxiosProvider>
+        <FavProvider>
+          <CartProvider>
+            <App></App>
+          </CartProvider>
+        </FavProvider>
+      </AxiosProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
