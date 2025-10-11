9fff1bf19e44309c850b5b29a723a9d98a44938c
shaghayegh <bazrafkan.sh@gmail.com>
2025-10-11 19:02:06 +0330

    refactor(category): modularize category styles and refactor component for scalability
    - moved Category styles from Home.css to a dedicated Categorys.css file
    - replaced repetitive category buttons with dynamic rendering using PRODUCT_CATEGORIES constant
    - added responsive behavior to hide icons on mobile in Products page
    - improved component readability and maintainability

--- src/components/Categorys/Categorys.jsx
+++ src/components/Categorys/Categorys.jsx
@@ -1,3 +1,6 @@
+import { useLocation } from "react-router-dom";
+import { memo, useState, useEffect } from "react";
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -8,17 +11,32 @@ import "swiper/css/navigation";
 // import required modules
 import { Scrollbar } from "swiper/modules";
 
-import { GiLargeDress, GiLipstick, GiPoloShirt, GiSkirt } from "react-icons/gi";
-import { PiDressFill } from "react-icons/pi";
-import { PiHandbagSimpleBold } from "react-icons/pi";
-import { TbHomeHeart ,TbHeartDiscount} from "react-icons/tb";
-import { FaSnowflake, FaGlasses, FaPersonHalfDress } from "react-icons/fa6";
-import { AiOutlineProduct } from "react-icons/ai";
+import { PRODUCT_CATEGORIES } from "../../constants/categories";
 import { useAxios } from "../../context/AxiosContaext/AxiosContaext";
-import { memo } from "react";
 
 function Categorys() {
-const { funcAxios, applyFilter, setSortFilter, setOnlyAvailable } = useAxios();
+  const { funcAxios, applyFilter, setSortFilter, setOnlyAvailable } =
+    useAxios();
+  const location = useLocation();
+  const [hideIcons, setHideIcons] = useState(false);
+
+  useEffect(() => {
+    const checkHideIcons = () => {
+      const isMobile = window.innerWidth < 768;
+      const isProductsPage = location.pathname === "/Fotros/Products";
+      setHideIcons(isMobile && isProductsPage);
+    };
+
+    checkHideIcons();
+    window.addEventListener("resize", checkHideIcons);
+    return () => window.removeEventListener("resize", checkHideIcons);
+  }, [location.pathname]);
+  const handleCategoryClick = (url, filterName) => {
+    funcAxios(url);
+    setSortFilter("");
+    setOnlyAvailable(false);
+    applyFilter("", false, filterName);
+  };
 
   return (
     <>
@@ -33,219 +51,27 @@ const { funcAxios, applyFilter, setSortFilter, setOnlyAvailable } = useAxios();
             768: { slidesPerView: 5 },
             1000: { slidesPerView: 7 },
             1200: { slidesPerView: 8 },
-            1600:{ slidesPerView: 10},
-            1800:{ slidesPerView: 11},
-            2500:{ slidesPerView: 20},
-
+            1600: { slidesPerView: 10 },
+            1800: { slidesPerView: 11 },
+            2500: { slidesPerView: 20 },
           }}
           modules={[Scrollbar]}
         >
-          <SwiperSlide>
-            <button
-              type="button"
-              onClick={() => {
-                funcAxios(
-                  "https://686b9bdee559eba90873470f.mockapi.io/ap/bazrafkan-store/products?sortBy=idsortby&order=desc"
-                );
-                setSortFilter("");
-    setOnlyAvailable(false);
-    applyFilter("", false, "همه محصولات");
-              }}
-              className={`Category`}
-            >
-              <AiOutlineProduct /> <p>همه محصولات</p>
-            </button>
-          </SwiperSlide>
-          <SwiperSlide>
-            <button
-              type="button"
-              onClick={() => {
-                funcAxios(
-                  "https://686b9bdee559eba90873470f.mockapi.io/ap/bazrafkan-store/products?category=مانتو"
-                );
-                setSortFilter("");
-      setOnlyAvailable(false);
-      applyFilter("", false, "مانتو");
-              }}
-              className={`Category`}
-            >
-              <GiPoloShirt /> <p>مانتو</p>
-            </button>
-          </SwiperSlide>
-          <SwiperSlide>
-            <button
-              type="button"
-              onClick={() => {
-                funcAxios(
-                  "https://686b9bdee559eba90873470f.mockapi.io/ap/bazrafkan-store/products?category=شلوار"
-                );
-               setSortFilter("");
-      setOnlyAvailable(false);
-      applyFilter("", false, "شلوار و دامن");
-              }}
-              className={`Category`}
-            >
-              <GiSkirt />
-              <p>
-                شلوار{"\u2009"}و{"\u2009"}دامن
-              </p>
-            </button>
-          </SwiperSlide>
-          <SwiperSlide>
-            <button
-              type="button"
-              onClick={() => {
-                funcAxios(
-                  "https://686b9bdee559eba90873470f.mockapi.io/ap/bazrafkan-store/products?category=ست"
-                );
-                setSortFilter("");
-      setOnlyAvailable(false);
-      applyFilter("", false, "ست");
-              }}
-              className={`Category`}
-            >
-              <PiDressFill />
-              <p>ست</p>
-            </button>
-          </SwiperSlide>
-          <SwiperSlide>
-            <button
-              type="button"
-              onClick={() => {
-                funcAxios(
-                  "https://686b9bdee559eba90873470f.mockapi.io/ap/bazrafkan-store/products?category=خونگی"
-                );
-                setSortFilter("");
-      setOnlyAvailable(false);
-      applyFilter("", false, "تو خونه ای");
-              }}
-              className={`Category`}
-            >
-              <TbHomeHeart />
-              <p>
-                تو{"\u2009"}خونه{"\u200A"}ای
-              </p>
-            </button>
-          </SwiperSlide>
-          <SwiperSlide>
-            <button
-              type="button"
-              onClick={() => {
-                funcAxios(
-                  "https://686b9bdee559eba90873470f.mockapi.io/ap/bazrafkan-store/products?category=ارایشی"
-                );
-               setSortFilter("");
-      setOnlyAvailable(false);
-      applyFilter("", false, "تو خونه ای");
-              }}
-              className={`Category`}
-            >
-              <GiLipstick />
-
-              <p>ارایشی&zwnj;&zwnj;بهداشتی</p>
-            </button>
-          </SwiperSlide>
-          <SwiperSlide>
-            <button
-              type="button"
-              onClick={() => {
-                funcAxios(
-                  "https://686b9bdee559eba90873470f.mockapi.io/ap/bazrafkan-store/products?category=زمستونه"
-                );
-               setSortFilter("");
-      setOnlyAvailable(false);
-      applyFilter("", false, "لباس گرم");
-              }}
-              className={`Category`}
-            >
-              <FaSnowflake />
-              <p>لباس{"\u200A"}گرم</p>
-            </button>
-          </SwiperSlide>
-          <SwiperSlide>
-            <button
-              type="button"
-              onClick={() => {
-                funcAxios(
-                  "https://686b9bdee559eba90873470f.mockapi.io/ap/bazrafkan-store/products?category=کیف"
-                );
-                setSortFilter("");
-      setOnlyAvailable(false);
-      applyFilter("", false, "لباس گرم");
-              }}
-              className={`Category`}
-            >
-              <PiHandbagSimpleBold />{" "}
-              <p>
-                کیف{"\u2009"}و{"\u2009"}کفش
-              </p>
-            </button>
-          </SwiperSlide>
-          <SwiperSlide>
-            <button
-              type="button"
-              onClick={() => {
-                funcAxios(
-                  "https://686b9bdee559eba90873470f.mockapi.io/ap/bazrafkan-store/products?category=اکسسوری"
-                );
-               setSortFilter("");
-      setOnlyAvailable(false);
-      applyFilter("", false, "اکسسوری");
-              }}
-              className={`Category`}
-            >
-              <FaGlasses />
-              <p>اکسسوری</p>
-            </button>
-          </SwiperSlide>
-          <SwiperSlide>
-            <button
-              type="button"
-              onClick={() => {
-                funcAxios(
-                  "https://686b9bdee559eba90873470f.mockapi.io/ap/bazrafkan-store/products?category=مجلسی"
-                );
-                setSortFilter("");
-      setOnlyAvailable(false);
-      applyFilter("", false, "مجلسی");
-              }}
-              className={`Category`}
-            >
-              <GiLargeDress /> <p>لباس م&#x0640;ج&#x0640;لس&#x0640;ی</p>
-            </button>
-          </SwiperSlide>
-          <SwiperSlide>
-            <button
-              type="button"
-              onClick={() => {
-                funcAxios(
-                  "https://686b9bdee559eba90873470f.mockapi.io/ap/bazrafkan-store/products?sortBy=off&order=desc"
-                );
-                setSortFilter("");
-      setOnlyAvailable(false);
-      applyFilter("", false, "فروش ویژه");
-              }}
-              className={`Category`}
-            >
-              <TbHeartDiscount /> <p> ف&#x0640;روش وی&#x0640;&#x0640;ژه</p>
-            </button>
-          </SwiperSlide>
-          <SwiperSlide>
-            <button
-              type="button"
-              onClick={() => {
-                funcAxios(
-                  "https://686b9bdee559eba90873470f.mockapi.io/ap/bazrafkan-store/products?category=اسپورت"
-                );
-                setSortFilter("");
-      setOnlyAvailable(false);
-      applyFilter("", false, "اسپورت");
-              }}
-              className={`Category`}
-            >
-              <FaPersonHalfDress /> <p>اسپورت</p>
-            </button>
-          </SwiperSlide>
+          {PRODUCT_CATEGORIES.map(
+            ({ id, name, icon: Icon, url, filterName }) => (
+              <SwiperSlide key={id}>
+                <button
+                  type="button"
+                  onClick={() => handleCategoryClick(url, filterName)}
+                  className={`Category flex flex-col items-center justify-center py-[3px] px-[6px] w-[85px] h-[84px] md:w-[128px] md:h-[128px] lg:w-[140px] lg:h-[120px] bg-[#d8dce4] rounded-[1rem]
+                        ${hideIcons ? "w-[90px] h-[fit-content]" : ""}`}
+                >
+                  {!hideIcons && Icon && <Icon />}
+                  <p className="whitespace-nowrap">{name}</p>
+                </button>
+              </SwiperSlide>
+            )
+          )}
         </Swiper>
       </div>
     </>
